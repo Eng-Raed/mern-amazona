f8e55c079a5a3db7e22a2da7ce1f990b2088cfa8
Eng.RaedShkayyan <Eng.RaedShkayyan@gmail.com>
2023-07-06 03:41:23 +0300

    Video-05-List-Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
+import data from "./data";
 
 function App() {
   return (
-    <div >
-      <header >
+    <div>
+      <header>
         <a href="/">amazona</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/products/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/products/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>{product.price}</strong>
+                </p>
+                <button>add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
